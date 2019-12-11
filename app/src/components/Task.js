6292041c8b464609632c0b85814e00419eaf2e6b
Michael Schmid <michael.schmid@live.com>
2019-12-11 18:26:09 +0100

    Process indicators

--- app/src/components/Task.js
+++ app/src/components/Task.js
@@ -1,11 +1,14 @@
 const m = require("mithril");
 const store = require("../store");
 const Balance = require("./Balance");
+const Loader = require("./Loader");
 
 require("./Task.scss");
 
 const STATUS = {
+  PROCESSING: "PROCESSING",
   DONE: "DONE",
+  FAILED: "FAILED",
 };
 
 function Task(initialVnode) {
@@ -15,48 +18,45 @@ function Task(initialVnode) {
       const task = vnode.attrs;
       const numberOfUsers = store.users.length;
       const actualPointsMultiplier = (numberOfUsers - 1) / numberOfUsers;
-      if (task.status !== STATUS.DONE)
-        return (
-          <tr
-            style={
-              task.urgency > 1 &&
-              `background-color:rgb(255,97,97,${(task.urgency - 1) /
-                (store.tasks[0].urgency - 1)})`
-            }
-          >
-            <td>
-              <span class="room">{task.room}</span>
-              <span class="object">{task.object}</span>
-            </td>
-            <td class="points">
-              {actualPointsMultiplier &&
-                (
-                  task.points *
-                  actualPointsMultiplier *
-                  store.settings.pointNormaliser
-                ).toFixed(2)}
-            </td>
-            <td>
-              <button onclick={this.markAsDone.bind(task)}>Erledigt</button>
-            </td>
-          </tr>
-        );
-      // When done
+      // Initial
       return (
-        <tr>
+        <tr
+          style={
+            task.status !== STATUS.DONE &&
+            task.urgency > 1 &&
+            `background-color:rgb(255,97,97,${(task.urgency - 1) /
+              (store.tasks[0].urgency - 1)})`
+          }
+          class={task.status === STATUS.DONE && "done"}
+        >
           <td>
-            {task.room}: {task.object}
+            <span class="room">{task.room}</span>
+            <span class="object">{task.object}</span>
           </td>
-          <td colspan="5">
+          <td class="points">
             {actualPointsMultiplier &&
-              (task.pointsCredited * actualPointsMultiplier).toFixed(2)}{" "}
-            Punkte gutgeschrieben
+              (
+                task.points *
+                actualPointsMultiplier *
+                store.settings.pointNormaliser
+              ).toFixed(2)}
+          </td>
+          <td>
+            <button
+              onclick={this.markAsDone.bind(task)}
+              disabled={
+                task.status === STATUS.PROCESSING || task.status === STATUS.DONE
+              }
+            >
+              {task.status === STATUS.PROCESSING ? <Loader /> : "  ✔  "}
+            </button>
           </td>
         </tr>
       );
     },
 
     markAsDone: function() {
+      this.status = STATUS.PROCESSING;
       m.request({
         method: "POST",
         url: "/api/task/:key/done",
--- app/src/components/TaskList.js
+++ app/src/components/TaskList.js
@@ -1,9 +1,12 @@
 const m = require("mithril");
 const store = require("../store");
 const TaskComponent = require("../components/Task");
+const Loader = require("../components/Loader");
 const Task = require("../../../shared/model/Task");
 
 module.exports = {
+  error: false,
+
   oninit: function() {
     return m
       .request({
@@ -11,17 +14,28 @@ module.exports = {
         url: "/api/tasks",
         type: Task,
       })
-      .then(function(result) {
+      .then(result => {
         store.tasks = result.sort((a, b) => b.urgency - a.urgency);
       })
-      .catch(function(error) {
+      .catch(error => {
         if (error.code === 401) {
           m.route.set("/login");
         }
+        this.error = true;
       });
   },
 
   view: function() {
+    if (store.tasks.length === 0) {
+      if (!this.error)
+        return (
+          <div>
+            Lade 
+            <Loader />
+          </div>
+        );
+      return <div class="error">Konnte Aufgaben nicht laden</div>;
+    }
     return (
       <table class="task-list">
         {store.tasks.map(task => {
