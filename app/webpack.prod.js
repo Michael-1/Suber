65538d274575749a2c97c517caba38b8bf941177
Michael Schmid <michael.schmid@live.com>
2021-08-07 15:10:56 +0200

    Package updates

--- app/webpack.prod.js
+++ app/webpack.prod.js
@@ -1,6 +1,21 @@
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.common.js");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = merge(baseConfig, {
   mode: "production",
+  plugins: [
+    new FaviconsWebpackPlugin({
+      logo: "./sponge.svg",
+      favicons: {
+        appName: "Suber",
+        appleStatusBarStyle: "default",
+        icons: {
+          coast: false,
+          firefox: false,
+          yandex: false,
+        },
+      },
+    }),
+  ],
 });
